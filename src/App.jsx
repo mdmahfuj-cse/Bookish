8d18eba4c98b0ac213f9ffd1e3bd8403f38acd7f
Mahfuj <mdmahfuj.cse@gmail.com>
2026-08-17 00:09:26 +0600

    add new product

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+
+      <div className="ticks"> hi </div>
+
+    </>
+  )
+}
+
+export default App
